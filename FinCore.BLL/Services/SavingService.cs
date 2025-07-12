21ef524cdb6686bb193687eaef2482d20b309078
Kaan Uçarcı <info@kaanucarci.com>
2025-07-12 23:04:22 +0300

    BLL layer is finished. Abstract service class created for common functions.

--- FinCore.BLL/Services/SavingService.cs
+++ FinCore.BLL/Services/SavingService.cs
@@ -1,6 +1,52 @@
+using FinCore.BLL.Interfaces;
+using FinCore.Entities.Models;
+
 namespace FinCore.BLL.Services;
 
-public class SavingService
+public class SavingService :
+    FinanceEntryServiceBase<Saving>, ISavingService
 {
-    
+    private readonly IRepository<Saving> _repo;
+    private readonly IRepository<Budget> _budgetRepo;
+
+    public SavingService(IRepository<Saving> repo, IRepository<Budget> budgetRepo) : base(repo, budgetRepo)
+    {
+        _repo = repo;
+        _budgetRepo = budgetRepo;
+    }
+
+    public async Task<List<Saving>> GetAllAsync(int budgetId)
+    {
+        await ValidateBudgetAsync(budgetId);
+        return (await _repo.GetAllAsync()).Where(e => e.BudgetId == budgetId).ToList();
+    }
+
+
+    public async Task<Saving?> GetByIdAsync(int id)
+        => await _repo.GetByIdAsync(id);
+
+    public async Task AddAsync(Saving saving, int budgetId)
+    {
+        await FinanceEntryValidateAsync(saving.Amount, budgetId);
+        await _repo.AddAsync(saving);
+        await _repo.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Saving saving, int budgetId)
+    {
+        await FinanceEntryValidateAsync(saving.Amount, budgetId);
+        _repo.Update(saving);
+        await _repo.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Saving saving)
+    {
+        var exp = await _repo.GetByIdAsync(saving.Id);
+
+        if (exp is not null)
+        {
+            _repo.Delete(exp);
+            await _repo.SaveChangesAsync();
+        }
+    }
 }
--- FinCore.Entities/Models/Budget.cs
+++ FinCore.Entities/Models/Budget.cs
@@ -4,6 +4,11 @@ public class Budget : BaseEntity
 {
     public int Year { get; set; }
     public int Month { get; set; }
+    
+    //Total amount will always stay the same
+    public decimal TotalAmount { get; set; }
+    
+    //When the user adds a new expense or saving Amount will be updated
     public decimal Amount { get; set; }
 
     public virtual ICollection<Expense>? Expenses { get; set; }
