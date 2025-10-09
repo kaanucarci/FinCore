12d5e566de167ee23cc3fe1c3c9af73738461871
Kaan Uçarcı <info@kaanucarci.com>
2025-10-09 16:12:46 +0300

    Migrations.

---
+++ FinCore.DAL/Migrations/20251008203433_DropColumnUserName.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinCore.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropColumnUserName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "Email");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                newName: "IX_Users_Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Users",
+                newName: "UserName");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                newName: "IX_Users_UserName");
+        }
+    }
+}
